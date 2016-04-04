39a8645e714fbf03e82a98d90eb8b582b8eb6b41
ericslc <eric.leatham@gmail.com>
2016-04-04 14:06:03 -0600

    updated to show

---
+++ config/auth.js
@@ -0,0 +1,8 @@
+module.exports = {
+  'facebookAuth' : {
+       'clientID'      : '625506010937493', // your App ID
+       'clientSecret'  : 'a0cf3e11adc6f363813096dd1b2240ba', // your App Secret
+       'callbackURL'   : 'http://localhost:8080/auth/facebook/callback'
+
+}
+};
